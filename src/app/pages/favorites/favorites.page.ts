f1a182dc7369dd1cc132044328ebd7d5d61c18cc
Peter Vacho <p_vacho@utb.cz>
2024-01-19 00:39:31 +0100

    Reload favorites in view enter

--- src/app/pages/favorites/favorites.page.ts
+++ src/app/pages/favorites/favorites.page.ts
@@ -15,29 +15,48 @@ import { OpenlibraryApiService } from 'src/app/services/openlibrary-api/openlibr
   styleUrls: ['./favorites.page.scss'],
 })
 export class FavoritesPage implements OnInit {
-  private favoriteEditionIds$: Observable<string[]>;
+  private favoriteEditionIds$!: Observable<string[]>;
 
   // Pagination
   private curIndex: number = 0;
   private fetchSize: number = 20;
-  private keepFetcing: boolean = true;
+  private keepFetcing: boolean = false;
   private fetchedEditionsSubject = new BehaviorSubject<EditionModel[]>([]);
   fetchedEditions$: Observable<EditionModel[]> =
     this.fetchedEditionsSubject.asObservable();
+  protected eventTarget: HTMLIonInfiniteScrollElement | null = null;
 
   constructor(
     private favoritesService: FavoritesService,
-    private openLibraryApiService: OpenlibraryApiService,
-  ) {
-    this.favoriteEditionIds$ = from(favoritesService.getFavorites()).pipe(
-      map((item) => Array.from(item)),
-    );
-
-    // Perform the initial (ion-infinite-scroll won't trigger it on it's own)
-    this.loadEditions();
-  }
+    private openLibraryApiService: OpenlibraryApiService
+  ) { }
 
   ngOnInit() { }
+
+  /** This method will automatically get triggered once the page is shown/entered.
+   *
+   * This is different to the page loading, as this page may have just been "paused",
+   * by the user being in another tab, from which they've added another favorite item
+   * For that reason, we'll need to reload the list of those here.
+   *
+   * That said, this function will still run also on the page load, not just on re-enter.
+   * That means this will also perform the initial load.
+   */
+  ionViewWillEnter() {
+    this.favoriteEditionIds$ = from(this.favoritesService.getFavorites()).pipe(
+      map((x) => Array.from(x))
+    );
+
+    this.favoriteEditionIds$.subscribe(() => {
+      this.fetchedEditionsSubject.next([]);
+      this.curIndex = 0;
+      this.keepFetcing = true;
+      if (this.eventTarget != null) {
+        this.eventTarget.disabled = false;
+      }
+      this.loadEditions();
+    });
+  }
 
   private async loadEditions() {
     if (this.keepFetcing == false) {
@@ -52,14 +71,14 @@ export class FavoritesPage implements OnInit {
 
     const toFetch = editionIds.slice(
       this.curIndex,
-      Math.min(this.curIndex + this.fetchSize, editionIds.length),
+      Math.min(this.curIndex + this.fetchSize, editionIds.length)
     );
 
     this.curIndex += this.fetchSize;
 
     // Await all of the API call results at once
     const promises = toFetch.map((id) =>
-      firstValueFrom(this.openLibraryApiService.get_edition$(id)),
+      firstValueFrom(this.openLibraryApiService.get_edition$(id))
     );
     const results = await Promise.all(promises);
 
@@ -78,6 +97,11 @@ export class FavoritesPage implements OnInit {
     // Check if all editions are loaded
     if (this.keepFetcing == false) {
       event.target.disabled = true;
+    }
+
+    // Store the event's target, so that we can re-enable it if needed
+    if (this.eventTarget == null) {
+      this.eventTarget = event.target;
     }
   }
 }
