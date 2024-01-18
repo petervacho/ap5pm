870b652f1c4d53b88e3ee84f73e72056bec55d34
Peter Vacho <p_vacho@utb.cz>
2024-01-18 21:37:41 +0100

    Add android platform for capacitor
    Auto-generated with `ionic cap add android`

---
+++ android/app/src/main/java/com/peter_vacho/book_explorer/MainActivity.java
@@ -0,0 +1,5 @@
+package com.peter_vacho.book_explorer;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,12 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'AP5PM',
+  appId: 'com.peter_vacho.book_explorer',
+  appName: 'Book Explorer',
   webDir: 'www',
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: 'https',
+  },
 };
 
 export default config;
